61d43eecb2a3b9f6b08e3f8a92f3b8449cf75806
calebcaruso <calebcaruso@gmail.com>
2026-09-07 03:32:06 +0800

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, padding: 0, backgroundColor: '#020617' }}>
+        {children}
+      </body>
+    </html>
+  );
+}
