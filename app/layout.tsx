b567fc3477fdb2f252487cd93eb5a039708b57f3
calebcaruso <calebcaruso@gmail.com>
2026-09-07 03:50:38 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './globals.css';
 
 export default function RootLayout({
   children,
@@ -8,6 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
       <body className="bg-zinc-950 text-zinc-100 min-h-screen">
         {children}
       </body>
